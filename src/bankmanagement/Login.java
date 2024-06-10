9fc0b9b4e4b75757848eb8904cee7d453d19bea4
jucanik <anik.patra2014@gmail.com>
2024-06-11 00:08:01 +0530

    Add Bank Management System Desktop Application Apache Ant Build Files

---
+++ src/bankmanagement/Login.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankmanagement;
+import javax.swing.*;
+/**
+ *
+ * @author jucanik
+ */
+public class Login extends JFrame{
+    
+}
